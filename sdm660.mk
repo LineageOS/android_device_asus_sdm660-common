33b1d246fab4bdda9d271ad4bf36fcb921931b04
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-08 23:40:58 +0200

    sdm660-common: Retrofit dynamic partitions
    * Merge system and vendor, giving us a whopping ~5GB of space, pretty nice
    * Mark standard block devices as super block devices (from https://source.android.com/devices/tech/ota/dynamic_partitions/implement?hl=en#selinux-legacy)
    
    Change-Id: Ib8c8ea5ea5d93071097e79ae60fc98a72096aa1a

--- sdm660.mk
+++ sdm660.mk
@@ -221,6 +221,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
  PRODUCT_PACKAGES += \
      vendor_bt_firmware_mountpoint \
      vendor_dsp_mountpoint \
