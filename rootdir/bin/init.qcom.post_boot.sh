6abf04d0dcc29c8588ffdefca78512b1ac3d07a6
Vivekachooz <vivekachooz@gmail.com>
2023-08-20 11:25:39 +0530

    sdm660-common: Explicitly disable cdsprpcd for sdm636
    Change-Id: Ie67bd4ce11e327b96c3b141361940175532dcaa1

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -2545,6 +2545,12 @@ case "$target" in
             start vendor.cdsprpcd
         esac
 
+        # Disable cdsprpcd daemon for sdm636
+        case "$soc_id" in
+            "345" )
+            setprop vendor.fastrpc.disable.cdsprpcd.daemon 1
+        esac
+
         #Apply settings for sdm630 and Tahaa
         case "$soc_id" in
             "318" | "327" | "385" )
