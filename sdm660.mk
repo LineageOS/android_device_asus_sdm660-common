ed6a332d95d287dbbc3c6872d935811eb97114b9
Michael Bestas <mkbestas@gmail.com>
2024-09-11 00:04:13 +0300

    sdm660-common: Shim libwvhidl with libcrypto_shim
    The following change
    google/boringssl@e202e51
    removed symbols that our libs require.
    
    Change-Id: I1325c34ecec3d339dd68c665010ecb36fa2e6a29

--- sdm660.mk
+++ sdm660.mk
@@ -133,7 +133,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    libcrypto_shim.vendor
 
 # Fingerprint
 PRODUCT_PACKAGES += \
