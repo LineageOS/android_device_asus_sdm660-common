0cd1cab0a01ec37a6e8e4c0579982cd8ad25f6ba
Michael Bestas <mkbestas@lineageos.org>
2025-01-04 09:09:32 +0200

    sdm660-common: Remove OMX audio codecs
     * OMX HW audio codecs won't work without QCOM A/V enhancements, which
       we don't have on Lineage.
     * OMX SW audio codecs no longer exist.
    
    Change-Id: I940be6716b603e848c44839bb87ad690eb55e86f

--- sdm660.mk
+++ sdm660.mk
@@ -233,7 +233,6 @@ PRODUCT_PACKAGES += \
 # Media configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media/media_profiles_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml
 
@@ -269,12 +268,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
     libmm-omxcore \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxG711Enc \
-    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw
