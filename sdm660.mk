269cf92994f22a1e68043c64a0595c615caab4b8
Bruno Martins <bgcngm@gmail.com>
2025-04-29 23:37:50 +0100

    sdm660-common: Migrate Lineage Health to soong_config_set_bool
    Change-Id: I2eff39e83b9673aa2989d05de65c646241ba61da

--- sdm660.mk
+++ sdm660.mk
@@ -221,6 +221,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
+
 # LiveDisplay native
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
