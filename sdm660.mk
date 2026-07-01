9f8b9c263b996f3bf1691e15cfa66138beb0b5e7
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-01 01:33:26 -0400

    sdm660-common: Use legacy libion implementation
    * At least one blob directly depends on this, with
      many other bits and pieces likely.
    
    Change-Id: Ic063618d6942a4f6e90dd7e21698e36d14113d06

--- sdm660.mk
+++ sdm660.mk
@@ -141,6 +141,9 @@ $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):libinit_sdm660)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl
 
+# ION
+$(call soong_config_set_bool,libion,legacy_impl,true)
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
